943296af92b8c275f785aa7b41c55be7be85c6fd
admin <1562mawa@gmail.com>
2023-02-19 18:23:58 +0200

    comit

--- scripts/index.js
+++ scripts/index.js
@@ -10,10 +10,10 @@ div.style.widht = "100px";
 div.style.height = "100px";
 div.style.backgroundColor = "blue";
 
-// div.addEventListener("mouseover", function (){
-//     // const color = prompt("Введите цвет в формате #......")
-//     div.style.backgroundColor = color;
-// });
+div.addEventListener("mouseover", function (){
+    const color = prompt("Введите цвет в формате #......")
+    div.style.backgroundColor = color;
+});
 div.addEventListener("mouseover", function(){
     const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
     div.style.backgroundColor = randomColor;
